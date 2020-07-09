f953ff0c001bb3ecb4c8000c3879c95e90f2688f
LinaFawn <tcipkoalina2000@mail.ru>
2020-07-09 12:48:27 +0300

    swagger

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -4,16 +4,20 @@ import com.example.dto.UserDTO;
 import com.example.exception.ResourceNotFoundException;
 import com.example.model.UserModel;
 import com.example.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/tables")
+@Api(value = "UserController - catch all url linking with user")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @PostMapping("/users")
+    @ApiOperation(value = "Add User")
     public int addUser(@RequestBody UserDTO userDto) {
         String username = userDto.getName();
         String email = userDto.getEmail();
@@ -22,6 +26,7 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
+    @ApiOperation(value = "Get User by Id")
     public UserDTO getUserById(@PathVariable(value = "id") Integer userId)
             throws ResourceNotFoundException {
         UserModel userModel = this.userService.getUserById(userId);
